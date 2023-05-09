65ed1362bb76395a3ba1f74b154e2fc35250b917
Timo <tifauss@gmail.com>
2023-05-09 12:58:34 +0200

    Auth

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,6 @@
 /* eslint-disable eol-last */
 module.exports = {
   attributes: {
-    name: { type: 'string', required: true },
     isPremium: { type: 'boolean', required: false},
     birthday: {  type: 'ref', columnType: 'datetime' },
     note: { type: 'string', required: false },
