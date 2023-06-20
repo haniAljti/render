4883df9fd9471d20bb5a765c5654c8b3d3e02646
Mhd Hani Alalajati <mh121ala@htwg-konstanz.de>
2023-06-20 12:34:29 +0200

    Update Participant.js

--- api/models/Participant.js
+++ api/models/Participant.js
@@ -1,11 +1,14 @@
 module.exports = {
     attributes: {
         sessionId: {
-            model: 'startedQuiz'
+            model: 'startedQuiz',
+            unique: false,
+            required: true
         },
         user: {
             model: 'user',
-            unique: true
+            unique: true,
+            required: true
         },
         score: { type: 'number', defaultsTo: 0 } 
     }
